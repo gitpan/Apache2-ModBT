5ae1f67716f34bcdb8c5c9137d59b7276866b084
Tyler MacDonald <japh@crackerjack.net>
2006-04-21 22:38:53 +0000

    Import of CRAKRJACK/Apache2-ModBT-0.010 from CPAN.
    gitpan-cpan-distribution: Apache2-ModBT
    gitpan-cpan-version:      0.010
    gitpan-cpan-path:         CRAKRJACK/Apache2-ModBT-0.010.tar.gz
    gitpan-cpan-author:       CRAKRJACK
    gitpan-cpan-maturity:     released

---
+++ ModBT.xs
@@ -0,0 +1,54 @@
+#include "mod_perl.h"
+#include "modperl_xs_typedefs.h"
+
+
+#include "EXTERN.h"
+#include "perl.h"
+#include "XSUB.h"
+
+#include "ppport.h"
+#include "libbtt/bt_types.h"
+#include "apache2/mod_bt.h"
+#include "apr.h"
+#include "apr_strings.h"
+#include "apr_hash.h"
+#include "apr_general.h"
+#include "apr_portable.h"
+#include "apr_lib.h"
+#include "apr_user.h"
+#include "httpd.h"
+#include "http_config.h"
+#include "http_log.h"
+#include "http_request.h"
+#include "http_protocol.h"
+#include "http_core.h"
+#include "util_filter.h"
+#include "mpm.h"
+#include "apr_dbm.h"
+#include "apr_optional.h"
+#define APR_WANT_STRFUNC
+#include "apr_want.h"
+#include "apr_signal.h"
+#include "apr_pools.h"
+
+MODULE = Apache2::ModBT		PACKAGE = Apache2::ModBT
+
+Net::BitTorrent::LibBTT::Tracker
+ModBT_Tracker(server)
+	Apache2::ServerRec server
+	
+	CODE:
+	perltracker *rv;
+	modbt_config_t *cfg = ap_get_module_config(server->module_config, &bt_module);
+
+	New(0, rv, 1, perltracker);
+	
+	apr_pool_create(&rv->p, cfg->tracker->p);
+	rv->master = -1;
+	rv->tracker = cfg->tracker;
+
+	RETVAL = (Net__BitTorrent__LibBTT__Tracker) rv;
+	
+	OUTPUT:
+	RETVAL
+	
---
+++ lib/Apache2/ModBT/LeechNuker.pm
@@ -0,0 +1,106 @@
+#!perl
+
+package Apache2::ModBT::LeechNuker;
+
+=pod
+
+=head1 NAME
+
+Apache2::ModBT::LeechNuker - mod_perl handler to shun leeches on a mod_bt BitTorrent Tracker
+
+=head1 SYNOPSIS
+
+	PerlModule Apache2::ModBT::LeechNuker
+
+	<Location "/announce">
+		<IfModule mod_perl.c>
+			PerlFixupHandler	Apache::ModBT::LeechNuker
+			PerlSetVar			MinLeechTime		3600
+		</IfModule>
+	</Location>
+
+=head1 DESCRIPTION
+
+The Apache::ModBT::LeechNuker module is a basic example on how to manipulate the operations of
+a mod_bt BitTorrent tracker from mod_perl.
+
+When run on an "/announce" request, this module will look up the peer making the request in
+the tracker's database. If the peer has not uploaded anything, and has been connected to
+the tracker for a certain amount of time (the default is 3600 seconds or 1 hour), has been
+downloading, and there are other peers that need his or her help, the peer's "Shunned"
+flag is set in the database.
+
+A "Shunned" peer is never given any other peers when it makes /announce requests, and is
+never served to other peers, effectively blocking the peer from the network 
+
+=head1 SEE ALSO
+
+L<Apache2::ModBT>, L<Net::BitTorrent::LibBTT>, L<http://www.crackerjack.net/mod_bt/>, L<http://perl.apache.org/>
+
+=cut
+
+use strict;
+use warnings;
+
+use Apache2::Const qw(DECLINED);
+use Apache2::RequestRec;
+use Apache2::RequestIO;
+
+use URI;
+use Apache2::ModBT;
+use Net::BitTorrent::LibBTT;
+use URI::Escape;
+
+our $Shunned;
+
+my(%flags)=(reverse(Net::BitTorrent::LibBTT::Peer::Flags()));
+
+if(!$flags{Shunned})
+{
+ die "This tracker does not have a \"Shunned\" flag!";
+}
+
+$Shunned = $flags{Shunned};
+
+our $shunstr =
+ "Shunning peer \"%s\" from hash \"%s\" (No up, %u down, %u left, %u online, %u/%u downloaders)\n";
+
+return 1;
+
+sub handler
+{
+ my $r = shift;
+ my $uri = URI->new();
+ my $leechtime = $r->dir_config("MinLeechTime") || 3600;
+ $uri->query($r->args);
+ my(%args)=($uri->query_form());
+ my $tracker = $r->server->ModBT_Tracker();
+ 
+ if($args{"info_hash"} && $args{"peer_id"})
+ {
+  if(my $hash = $tracker->Infohash(uri_unescape($args{"info_hash"})))
+  {
+   if(my $peer = $hash->Peer(uri_unescape($args{"peer_id"})))
+   {
+    if
+    (
+     (!$peer->uploaded) && ($peer->downloaded) && ($peer->left) &&
+     ($hash->seeds) && ($hash->peers > ($hash->seeds + 1)) &&
+     ((time() - $peer->first_t) > $leechtime) &&
+     (!($peer->flags & $Shunned))
+    )
+    {
+     warn sprintf
+     (
+      $shunstr, uri_escape($peer->peerid), uri_escape($peer->infohash),
+      $peer->downloaded, $peer->left, time() - $peer->first_t, $hash->peers, $hash->seeds
+     );
+     $peer->flags($peer->flags | $Shunned);
+     $peer->save();
+    }
+   }
+  }
+ }
+ 
+ return DECLINED;
+}
